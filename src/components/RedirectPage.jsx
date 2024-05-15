b5503fa194ecb86bfce07f393114e68115e50db5
aswanidevan <66940827+aswanidevan@users.noreply.github.com>
2024-05-16 03:32:15 +0530

    Commit before Deploying (minor edits)

--- src/components/RedirectPage.jsx
+++ src/components/RedirectPage.jsx
@@ -1,33 +1,43 @@
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { dotWave } from 'ldrs'
 
+
+//get ENV VARIABLE
 const apiURL = import.meta.env.VITE_API_URL;
 
 function RedirectPage() {
+  //get params for redirect
   const { shortUrl } = useParams();
   const navigate = useNavigate();
+  dotWave.register()
 
   useEffect(() => {
     const fetchOriginalUrl = async () => {
       try {
-        const response = await axios.get(`${apiURL}/api/${shortUrl}`);
+        const response = await axios.get(`${apiURL}/api/${shortUrl}`);  //Fetch original URL from API
         const {linkExist, originalUrl,msg } = response.data;
-        if(linkExist){
-        window.location.href = originalUrl;
-      }
-else  navigate('/', { state: { msg: msg } });
+        if(linkExist)  window.location.href = originalUrl;            //Redirect to original URL
+        else  navigate('/', { state: { msg: msg } });                 //Go to Home with msg
       }
       catch (error) {
         console.error('Error:', error);
-        navigate('/',{state: {msg:"Unable to connect to API"}});
+        navigate('/',{state: {msg:"Unable to connect to API"}});      //Failed to fetch URL Go to Home with msg
       }
     };
 
     fetchOriginalUrl();
-  }, [shortUrl, navigate]);
+  }, [shortUrl]);
 
-  return <p className='fs-bolder fs-1 text-center color-header-title py-5 text-shadow-light'>Redirecting...</p>;
+  return (<>
+  
+
+  <p className='fs-bolder fs-1 text-center color-header-title py-5 text-shadow-light'>Redirecting<l-dot-wave
+  size="45"
+  speed="0.9" 
+  color="#38cf99" 
+></l-dot-wave></p></>); 
 }
 
 export default RedirectPage;
--- src/components/urlcomponents/UrlShortnerApp.jsx
+++ src/components/urlcomponents/UrlShortnerApp.jsx
@@ -6,7 +6,6 @@ import { useLocation } from 'react-router-dom';
 import {  toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 export const apiURL =import.meta.env.VITE_API_URL;
 
 function UrlShortnerApp() {
@@ -16,24 +15,26 @@ function UrlShortnerApp() {
     urlSize: '5'
   });
 
+//State to manage Recieved Urls
+  const [urlsShortened, setUrlsShortened] = useState([]);
+
+  //Get Msg From Redirect Page on Failure
   const location = useLocation();
-
-
+  //Toast Msg from Redirect Page on Failure
   useEffect(() => {
     if (location.state && location.state.msg) {
       toast(location.state.msg);
       location.state.msg='';
     }
-
   }, [location]);
  
+
+  //Create a Toast
   const notify=   (toastMsg) => {
     toast(toastMsg);
   }
 
-  const [urlsShortened, setUrlsShortened] = useState([]);
-
-  // Handler function to update form data
+  // Handler function to update formdata
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
@@ -45,8 +46,9 @@ function UrlShortnerApp() {
     try {
       // Send form data to the local host using Axios
       const response = await axios.post(`${apiURL}/api/shortenUrl`, formData);
-      const {msg}=response.data
+      const {msg,isShortened}=response.data
       notify( msg);
+      if(isShortened)
       setUrlsShortened([...urlsShortened, response.data]);      
     } 
     catch (error) {
